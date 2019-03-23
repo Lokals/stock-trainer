49089c075a9c54421f69e6696f85e322afe95fa1
Marcin Kowalewski <kowalewski.marcinn@gmail.com>
2019-03-23 13:43:03 +0100

    Initial commit

---
+++ src/main/java/pl/sdacademy/stocktrainer/StockTrainerApplication.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.stocktrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTrainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockTrainerApplication.class, args);
+    }
+
+}
